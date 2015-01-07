2adbb32ffbc90e951a16d66a183f3b0306585004
Ultramanhu <qchu03@hotmail.com>
2015-01-07 14:15:57 +0800

    test

---
+++ C#/testFS/testFS/Program.fs
@@ -0,0 +1,19 @@
+﻿// 在 http://fsharp.net 上了解有关 F# 的更多信息
+// 请参阅“F# 教程”项目以获取更多帮助。
+
+let add a b = a + b
+
+[<EntryPoint>]
+let main argv = 
+    let v = ()
+
+    let list = [1; 2; 3]
+
+    add 1 2 |> ignore
+    
+    @"<book title=""Paradise Lost"">"
+
+    printf "1"
+    
+    //printfn "%A" argv
+    0 // 返回整数退出代码
