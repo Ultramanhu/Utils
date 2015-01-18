d049335b960bf74a2058fcc967d15348e3315c28
ultramanhu <qchu03@foxmail.com>
2015-01-18 23:45:22 +0800

    1

---
+++ C#/Coroutine/Tester/Coroutine/Executor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coroutine {
+    public class Executor {
+        public void Start(IEnumerator arg) {
+
+        }
+    }
+}
---
+++ C#/Coroutine/Tester/Coroutine/YieldWaiter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coroutine {
+    public class YieldWaiter : IEnumerator {
+
+        public object Current {
+            get {
+                return this;
+            }
+        }
+
+        public bool MoveNext() {
+            return true;
+        }
+
+        public void Reset() {
+        }
+        
+
+    }
+}
