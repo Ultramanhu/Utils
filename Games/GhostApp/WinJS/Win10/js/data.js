dba4ee59eaf1ab4e31ffcf619aeba59321575f68
ultramanhu <qchu03@foxmail.com>
2015-09-05 22:06:27 +0800

    all

--- Games/GhostApp/WinJS/Win10/js/data.js
+++ Games/GhostApp/WinJS/Win10/js/data.js
@@ -1,89 +1,70 @@
-﻿
 /// <reference path="../jquery/jquery.d.ts" />
-
-enum IDENTITY {
-    ID_UNKNOWN = 0,
-    ID_MAJOR = 1,
-    ID_MINOR = 2,
-    ID_GHOST = 3,
-};
-
+var IDENTITY;
+(function (IDENTITY) {
+    IDENTITY[IDENTITY["ID_UNKNOWN"] = 0] = "ID_UNKNOWN";
+    IDENTITY[IDENTITY["ID_MAJOR"] = 1] = "ID_MAJOR";
+    IDENTITY[IDENTITY["ID_MINOR"] = 2] = "ID_MINOR";
+    IDENTITY[IDENTITY["ID_GHOST"] = 3] = "ID_GHOST";
+})(IDENTITY || (IDENTITY = {}));
+;
 var IdenDict = {
     ID_MAJOR: "多数派",
     ID_MINOR: "少数派",
     ID_GHOST: "鬼",
     ID_UNKNOWN: ""
 };
-
-function Rand(n: number): number {
+function Rand(n) {
     return Math.floor((Math.random() * n));
 }
-
-class Player {
-
-    public pos: number;
-    public iden: IDENTITY = IDENTITY.ID_UNKNOWN;
-    public words: string = "";
-
-    public constructor(pos: number) {
+var Player = (function () {
+    function Player(pos) {
+        this.iden = IDENTITY.ID_UNKNOWN;
+        this.words = "";
         this.pos = pos;
     }
-}
-
-type WordTuple = [string, string, string];
-
-class GameManager {
-
-    protected list: Array<WordTuple> = [];
-    protected current: WordTuple;
-    protected players: Array<Player> = [];
-    protected playerNums: [number, number, number, number];
-
-    public static Ins: GameManager = new GameManager();
-
-    constructor() {
+    return Player;
+})();
+var GameManager = (function () {
+    function GameManager() {
+        this.list = [];
+        this.players = [];
         this.init();
     }
-
-    public init(): void {
+    GameManager.prototype.init = function () {
+        var _this = this;
         $.ajax({
             url: "./js/words.js",
             contentType: "application/javascript",
             dataType: 'json',
-            success: (res) => {
-                this.list = res.words;
+            success: function (res) {
+                _this.list = res.words;
             },
-            error: (a, b, c) => {
+            error: function (a, b, c) {
                 alertMsg("获取词库数据失败");
             }
         });
-    }
-
-    public randWords(): void {
+    };
+    GameManager.prototype.randWords = function () {
         if (this.list.length == 0) {
             alertMsg("词库无词条，无法随机");
             return;
         }
-        var rand: number = Rand(this.list.length);
+        var rand = Rand(this.list.length);
         this.current = this.list[rand];
-    }
-
-    public setCurrent(major: string, minor: string, ghost: string): void {
+    };
+    GameManager.prototype.setCurrent = function (major, minor, ghost) {
         this.current = [major, minor, ghost];
-    }
-
-    public getCurrent(): WordTuple {
+    };
+    GameManager.prototype.getCurrent = function () {
         return this.current;
-    }
-
-    public setPlayerNums(major: number, minor: number, ghost: number): void {
-        var total: number = major + minor + ghost;
+    };
+    GameManager.prototype.setPlayerNums = function (major, minor, ghost) {
+        var total = major + minor + ghost;
         this.playerNums = [total, major, minor, ghost];
-    }
-
-    public doRand(): void {
+    };
+    GameManager.prototype.doRand = function () {
         this.players = [];
-        var idens: Array<IDENTITY> = [];
+        var idens = [];
         var cnt = 0, k = IDENTITY.ID_MAJOR, i = 0;
         for (i = 0; i < this.playerNums[0]; i++) {
             this.players.push(new Player(i + 1));
@@ -95,8 +76,8 @@ class GameManager {
             idens.push(k);
         }
         for (i = 0; i < this.players.length; i++) {
-            var pos: number = Rand(idens.length);
-            var iden: IDENTITY = idens[pos];
+            var pos = Rand(idens.length);
+            var iden = idens[pos];
             this.players[i].iden = iden;
             this.players[i].words = this.current[iden - 1];
             if (idens.length > 0) {
@@ -104,13 +85,14 @@ class GameManager {
                 idens.pop();
             }
         }
-    }
-
-    public getPlayerByPos(pos: number): Player {
+    };
+    GameManager.prototype.getPlayerByPos = function (pos) {
         return this.players[pos - 1];
-    }
-
-    public getTotle(): number {
+    };
+    GameManager.prototype.getTotle = function () {
         return this.playerNums[0];
-    }
-}
+    };
+    GameManager.Ins = new GameManager();
+    return GameManager;
+})();
+//# sourceMappingURL=data.js.map
--- Games/GhostApp/WinJS/Win10/js/ui.js
+++ Games/GhostApp/WinJS/Win10/js/ui.js
@@ -1,114 +1,106 @@
-﻿
 /// <reference path="../jquery/jquery.d.ts" />
 /// <reference path="../winjs/js/winjs.d.ts" />
-
 "use strict";
-
-const WORD_PREFIX = "player_word_";
-const IDEN_PREFIX = "player_iden_";
-
-
-var TPlayerDataSource: any = WinJS.Binding.define({
+var WORD_PREFIX = "player_word_";
+var IDEN_PREFIX = "player_iden_";
+var TPlayerDataSource = WinJS.Binding.define({
     show_iden: false,
     show_words: false,
     pos: 0,
-//    wordId: WORD_PREFIX,
-//    idenId: IDEN_PREFIX,
+    //    wordId: WORD_PREFIX,
+    //    idenId: IDEN_PREFIX,
     data: Player
 });
-
-var nowPosition: number = 0;
-var isAnimating: boolean = false;
-
-function alertMsg(msg: string) {
+var nowPosition = 0;
+var isAnimating = false;
+function alertMsg(msg) {
     $("#msg-content").text(msg);
     $("#common-msg")[0].winControl.show();
 }
-
 function toggleShowWords() {
     if (!$("#showWordsToggle")[0].winControl.checked) {
         $(".words-control").attr("type", "password").addClass("win-password");
-    } else {
-        $(".words-control").attr("type", "text").removeClass("win-password");;
+    }
+    else {
+        $(".words-control").attr("type", "text").removeClass("win-password");
+        ;
     }
 }
-
-function playerToggleWord(e: Event) {
-    var control: any = e.currentTarget;
+function playerToggleWord(e) {
+    var control = e.currentTarget;
     var playerId = control.playerId;
     if (control.winControl.checked) {
         var playerData = GameManager.Ins.getPlayerByPos(parseInt(playerId));
         $("#" + WORD_PREFIX + playerId).text(playerData.words);
-    } else {
+    }
+    else {
         $("#" + WORD_PREFIX + playerId).text("-");
     }
 }
-
 function playerToggleIden(e) {
-    var control: any = e.currentTarget;
+    var control = e.currentTarget;
     var playerId = control.playerId;
     if (control.winControl.checked) {
         var playerData = GameManager.Ins.getPlayerByPos(parseInt(playerId));
         $("#" + IDEN_PREFIX + playerId).text(IdenDict[IDENTITY[playerData.iden]]);
-    } else {
+    }
+    else {
         $("#" + IDEN_PREFIX + playerId).text("-");
     }
 }
-
 function randWords() {
     GameManager.Ins.randWords();
-    var words: WordTuple = GameManager.Ins.getCurrent();
+    var words = GameManager.Ins.getCurrent();
     $("#major_words").val(words[0]);
     $("#minor_words").val(words[1]);
     $("#ghost_words").val(words[2]);
 }
-
 function configNumChanges() {
-    var value: string = $("#config_nums").val();
-    var list: Array<string> = value.split("-");
+    var value = $("#config_nums").val();
+    var list = value.split("-");
     $("#major_nums").val(list[0]);
     $("#minor_nums").val(list[1]);
     $("#ghost_nums").val(list[2]);
 }
-
 WinJS.Namespace.define("GhostWinJS", {
-    cancel: WinJS.UI.eventHandler((ev) => {
-        if (isAnimating) return;
+    cancel: WinJS.UI.eventHandler(function (ev) {
+        if (isAnimating)
+            return;
         $("#pivotContent")[0].winControl.selectedIndex = 0;
         nowPosition = 0;
         isAnimating = true;
-        $("#gamingContent").fadeOut("fast", () => {
-            $("#pivotContent").fadeIn("fast", () => {
+        $("#gamingContent").fadeOut("fast", function () {
+            $("#pivotContent").fadeIn("fast", function () {
                 isAnimating = false;
                 $("#createAppBar")[0].winControl.getCommandById("cancel").hidden = true;
             });
         });
     }),
-    readme: WinJS.UI.eventHandler((ev) => {
+    readme: WinJS.UI.eventHandler(function (ev) {
         $("#help-msg")[0].winControl.show();
     })
 });
-
 function gamingShow() {
-    if (isAnimating) return;
+    if (isAnimating)
+        return;
     var info = GameManager.Ins.getPlayerByPos(nowPosition);
     $("#gaming_words").text(info.words);
     isAnimating = true;
-    $("#gamingHideContent").fadeOut("fast", () => {
-        $("#gamingShowContent").fadeIn("fast", () => {
+    $("#gamingHideContent").fadeOut("fast", function () {
+        $("#gamingShowContent").fadeIn("fast", function () {
             isAnimating = false;
         });
     });
 }
-
 function gamingHide() {
-    if (isAnimating) return;
+    if (isAnimating)
+        return;
     nowPosition = nowPosition + 1;
     if (nowPosition > GameManager.Ins.getTotle()) {
         $("#pivotContent")[0].winControl.selectedIndex = 1;
         isAnimating = true;
-        $("#gamingContent").fadeOut("fast", () => {
-            $("#pivotContent").fadeIn("fast", () => {
+        $("#gamingContent").fadeOut("fast", function () {
+            $("#pivotContent").fadeIn("fast", function () {
                 isAnimating = false;
                 $("#createAppBar")[0].winControl.getCommandById("cancel").hidden = true;
                 showState();
@@ -118,14 +110,13 @@ function gamingHide() {
     }
     $("#gaming_pos").text(nowPosition.toString());
     isAnimating = true;
-    $("#gamingShowContent").fadeOut("fast", () => {
-        $("#gamingHideContent").fadeIn("fast", () => {
+    $("#gamingShowContent").fadeOut("fast", function () {
+        $("#gamingHideContent").fadeIn("fast", function () {
             isAnimating = false;
         });
     });
 }
-
-var PlayerList: Array<any> = [];
+var PlayerList = [];
 function showState() {
     var container = $("#state_container");
     container.empty();
@@ -136,63 +127,51 @@ function showState() {
         var data = new TPlayerDataSource({
             show_iden: false,
             show_words: false,
-//            wordId: WORD_PREFIX + pos.toString(),
-//            idenId: IDEN_PREFIX + pos.toString(),
+            //            wordId: WORD_PREFIX + pos.toString(),
+            //            idenId: IDEN_PREFIX + pos.toString(),
             data: playerData
         });
         PlayerList.push(data);
-        template.render(data).then((e) => {
+        template.render(data).then(function (e) {
             $(".player_word", e).attr("id", WORD_PREFIX + pos.toString());
             $(".player_iden", e).attr("id", IDEN_PREFIX + pos.toString());
             container.append(e);
         });
     }
 }
-
 function startGame() {
-    var checker: (id: string, msg: string) => boolean = (id, msg) => {
+    var checker = function (id, msg) {
         if ($("#" + id).val() == "") {
             alertMsg(msg);
             $("#" + id).focus();
             return false;
         }
         return true;
-    }
-
-    if (!checker("major_words", "多数派词条未填写")) return;
-    if (!checker("minor_words", "少数派词条未填写")) return;
-    if (!checker("ghost_words", "鬼词条未填写")) return;
-    
-    GameManager.Ins.setCurrent(
-        $("#major_words").val(),
-        $("#minor_words").val(),
-        $("#ghost_words").val()
-    );
-    GameManager.Ins.setPlayerNums(
-        parseInt($("#major_nums").val()),
-        parseInt($("#minor_nums").val()),
-        parseInt($("#ghost_nums").val())
-    );
+    };
+    if (!checker("major_words", "多数派词条未填写"))
+        return;
+    if (!checker("minor_words", "少数派词条未填写"))
+        return;
+    if (!checker("ghost_words", "鬼词条未填写"))
+        return;
+    GameManager.Ins.setCurrent($("#major_words").val(), $("#minor_words").val(), $("#ghost_words").val());
+    GameManager.Ins.setPlayerNums(parseInt($("#major_nums").val()), parseInt($("#minor_nums").val()), parseInt($("#ghost_nums").val()));
     GameManager.Ins.doRand();
     nowPosition = 0;
     gamingHide();
-
-    $("#pivotContent").fadeOut("fast", () => {
-        $("#gamingContent").fadeIn("fast", () => {
+    $("#pivotContent").fadeOut("fast", function () {
+        $("#gamingContent").fadeIn("fast", function () {
             $("#createAppBar")[0].winControl.getCommandById("cancel").hidden = false;
         });
     });
 }
-
 function pageInit() {
     $("#createAppBar")[0].winControl.getCommandById("cancel").hidden = true;
 }
-
 WinJS.Utilities.markSupportedForProcessing(toggleShowWords);
 WinJS.Utilities.markSupportedForProcessing(playerToggleWord);
 WinJS.Utilities.markSupportedForProcessing(playerToggleIden);
-
-
-WinJS.UI.processAll().then(() => {
+WinJS.UI.processAll().then(function () {
     pageInit();
 });
+//# sourceMappingURL=ui.js.map
